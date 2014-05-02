0608e4fb87a9a1a66d4904d0f5a94d44f7245367
Danny Brown <danbrown@tournative.com>
2014-05-02 18:13:45 +0700

    change the id to represent better meaning

--- app/assets/javascripts/paloma/game.js
+++ app/assets/javascripts/paloma/game.js
@@ -3,13 +3,13 @@ var GameController = Paloma.controller('Game');
 GameController.prototype.new = function() {
     $(document).on("click", "#reset_moves", function(e) {
         $('#moves').html('');
-        $('#move_list').html("");
+        $('#left_hand_list').html("");
         $('#fight_button').hide();
     });
     $(document).on("click", ".game_acts", function(e) {
         if ($('.list-group-item').length < 10) {
             $('#moves').append($(this).data("name") + ",");
-            $('#move_list').append("<li class='list-group-item'>" + $(this).data("name") + "</li>");
+            $('#left_hand_list').append("<li class='list-group-item'>" + $(this).data("name") + "</li>");
         }
         if ($('.list-group-item').length == 10){
             $('#fight_button').show();
@@ -31,10 +31,11 @@ GameController.prototype.show = function() {
             $('#game_status').html("<h2>You win</h2>");
             $('#adversary_status').html("<h2>Your adversary loose</h2>");
         } else {
+            /* This is the Tie condition */
             $('#game_status').html("<h2>Tie</h2>");
             $('#adversary_status').html("<h2>Tie</h2>");
         }
-        $('#move_list').html("");
+        $('#left_hand_list').html("");
         for (var i=0;i< data.result.length;i++){
             var iwin = "";
             if (data.result[i] == "W"){
@@ -46,11 +47,11 @@ GameController.prototype.show = function() {
             if (data.result[i] == "L"){
                 iwin = "win";
             }
-            $('#move_list').append("<li class='list-group-item "+iwin+"'>"+data.waiter_moves[i]+"</li>");
+            $('#left_hand_list').append("<li class='list-group-item "+iwin+"'>"+data.waiter_moves[i]+"</li>");
         }
         $('.adversary_move').show();
-        var left_win = $('#move_list').find('li.win').length;
-        $('#move_list').after("<h2>"+left_win+"</h2>");
+        var left_win = $('#left_hand_list').find('li.win').length;
+        $('#left_hand_list').after("<h2>"+left_win+"</h2>");
         var right_win = $('#adversary_moves').find('li.win').length;
         $('#adversary_moves').after("<h2>"+right_win+"</h2>");
     });
